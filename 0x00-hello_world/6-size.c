2428c8a6b9ac1c476d63b66fded0a34177c88f6c
abo1st <abo1st001@gmail.com>
2023-09-07 13:59:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - A program that prints size of various types on the computer it is compiled and run on
- * 
+ *
  * Return: Always 0 (Success)
  */
-int main(void)
+	int main(void)
 {
 	int a;
 	long int b;
