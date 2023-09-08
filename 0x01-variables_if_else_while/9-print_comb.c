819288bd61f5cbfe2412c70b10d96681467f8a42
abo1st <abo1st001@gmail.com>
2023-09-08 08:58:31 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return: Always 0.
@@ -13,11 +12,9 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar('\n');
 
 	return (0);
