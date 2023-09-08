efeb8ed786ba4131dc167e57dec05b749d2c88b2
abo1st <abo1st001@gmail.com>
2023-09-08 07:46:52 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (c = 0; c < 10; c++)
+		putchar((c % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
