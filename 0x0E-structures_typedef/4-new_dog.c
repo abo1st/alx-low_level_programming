f0b739304132c09ca88350716c5fa791dea5155e
abo1st <abo1st001@gmail.com>
2023-10-10 17:35:54 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,7 +6,7 @@
  * _strlen - This returns the lenght of a string
  * @s: pointer to s
  * Return: 0 on success
- */
+*/
 
 int _strlen(char *s)
 {
@@ -19,26 +19,29 @@ int _strlen(char *s)
 	}
 	return (count);
 }
+
 /**
  * _strcpy - This copies the string with \0 to the buffer
  * @dest: pointer to dest
  * @src: pointer to string
  * Return: the pointer to dest
+ *
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int a;
+	int i;
 
-	a = 0;
-	while (*(src + a) != '\0')
+	i = 0;
+	while (*(src + i) != '\0')
 	{
-		*(dest + a) = *(src + a);
-		a++;
+		*(dest + i) = *(src + i);
+		i++;
 	}
-	*(dest + a) = '\0';
+	*(dest + i) = '\0';
 	return (dest);
 }
+
 /**
  * new_dog - This creates a new dog
  * @name: name
