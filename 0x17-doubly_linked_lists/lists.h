404df02da63c670a7aed962f59b063ad1f2b4809
abo1st <abo1st001@gmail.com>
2023-12-07 18:46:45 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -9,15 +9,14 @@
  * @n: The integer.
  * @prev: points to the previous node
  * @next: points to the next node
- *
  * Description: doubly linked list node structure
  */
 
 typedef struct dlistint_s
 {
-        int n;
-        struct dlistint_s *prev;
-        struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
