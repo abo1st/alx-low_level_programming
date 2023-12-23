ac871af631ec48e146d965a0fc87e0b3ba661267
abo1st <abo1st001@gmail.com>
2023-12-22 20:11:59 -0800

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (value_copy == NULL)
 		return (0);
 
-	indexz = key_index((const unsigned char *)key, ht->size);
+	indexz = key_indexz((const unsigned char *)key, ht->size);
 	for (iz = indexz; ht->array[iz]; iz++)
 	{
 		if (strcmp(ht->array[iz]->key, key) == 0)
